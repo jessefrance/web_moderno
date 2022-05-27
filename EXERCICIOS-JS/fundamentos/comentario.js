5daea40c1d268bbccc78c2b6a45a0d90ae2b0310
jessefrance <jesseluis09@gmail.com>
2022-05-26 23:27:09 -0300

    Fundamentos

---
+++ EXERCICIOS-JS/fundamentos/comentario.js
@@ -0,0 +1,14 @@
+// Comentário de uma Linha
+console.log("Linha 1");
+
+/*
+Comentário de 
+múltiplas linhas
+*/
+console("Linha 2")
+
+/*
+ * Comentário de 
+ * Múltiplas linhas
+ */
+console.log("Linha 3");
---
+++ EXERCICIOS-JS/fundamentos/tipagemFraca.js
@@ -0,0 +1,14 @@
+let qualquer = "Legal"
+console.log(qualquer);
+console.log(typeof qualquer);
+
+qualquer = 3.123;
+console.log(qualquer);
+console.log(typeof qualquer);
+
+/* Evitae nome genéricos
+ * e siglas. Ex:
+ */
+let valor = ""
+let numero = 1
+let pqp = false //Produto Químico Perigoso... hahahaha
