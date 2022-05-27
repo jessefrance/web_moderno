1e946c5386b64abf1d14cd2465ff8f116d963d73
jessefrance <jesseluis09@gmail.com>
2022-05-27 17:33:32 -0300

    add news knowledge

---
+++ EXERCICIOS-JS/fundamentos/numeros.js
@@ -0,0 +1,16 @@
+const peso1 = 1.0
+const peso2 = Number("2.0")
+
+console.log(peso1, peso2);
+console.log(Number.isInteger(peso1));
+console.log(Number.isInteger(peso2));
+
+const avalaciacao1 = 9.871
+const avalaciacao2 = 6.871
+
+const total = avalaciacao1 * peso1 + avalaciacao2 * peso2
+const media = total / (peso1 + peso2)
+
+console.log(media.toFixed(2));
+console.log(media.toString(2)); // em Binário
+console.log(typeof media);
